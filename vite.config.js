bb99e004c528246bc9b8bc6226a181624ab469f3
Sebastian Setrini <sebastian.setrini@fiuni.edu.py>
2025-11-23 23:02:46 -0300

    configurar base para GitHub Pagues

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  //  Me daba error en esta parte entoces le puse el nombre de mi repositorio
+  base: '/lab-6_Git/',
   server: {
     port: 3000,
-    open: true
+    open: true,
   },
-  build: {
-    outDir: 'dist',
-    sourcemap: true
-  }
 })
